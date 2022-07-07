f658dcea40c7c317d86d987bc9a163e509f13538
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-07 23:51:41 +0300

    task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿// 1. Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+// Размер массива от 5 до 10
+// Числа в массива от -20 до 20
+
+
 
 namespace GB
 {
@@ -8,8 +14,26 @@ namespace GB
         {
 
             Console.Clear();
-            
-            
+            int[] array = new int[new Random().Next(5, 11)];
+            FillArray(array);
+            PrintArray(array);
+
+
+        }
+        static void FillArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(-20, 21);
+            }
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]} \t {array[i] * -1}");   //    \t - табуляция 
+            }
         }
     }
+
 }
